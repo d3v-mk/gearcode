3990487c370af27c1e1f95b1f37a22a3625f37c8
mktre <devsoulmk@gmail.com>
2023-04-27 22:46:56 -0300

    versao não está muito boa, push somente para salvar a versao

--- forum/views.py
+++ forum/views.py
@@ -2,38 +2,68 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
-from .models import Postagem
-from django.contrib.sessions.models import Session
-from django.utils import timezone
-from django.contrib.auth.models import User
+from .models import Area, Postagem
+from django.utils.text import slugify
+from django.http import HttpResponseForbidden
 
 
-
-def python_geral(request):
-    return render(request, 'python.html')
-
-def python_basico(request):
-    return render(request, 'fundamentos_basicos_python.html')
 
 @login_required
-def nova_postagem(request):
-    if request.method == 'GET':
-        return render(request, 'nova_postagem.html')
-    elif request.method == "POST":
+def criar_area(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        name = request.POST.get('nome')
+        description = request.POST.get('descricao')
+        slug = slugify(name)
+
+        nova_area = Area(name=name, title=title, description=description, slug=slug)
+        nova_area.save()
+
+        context = {
+            'nova_area': nova_area,
+        }
+        return render(request, 'area_detail.html', context)
+
+    return render(request, 'criar_area.html')
+
+
+
+
+
+
+
+
+@login_required
+def nova_postagem(request, area_slug):
+    try:
+        area = Area.objects.get(slug=area_slug)
+    except Area.DoesNotExist:
+        return redirect('criar_area')
+    
+    if request.method == 'POST':
         titulo = request.POST.get('titulo')
         conteudo = request.POST.get('conteudo')
-
         postagem = Postagem(
-        titulo=titulo,
-        conteudo=conteudo,
-        autor=request.user,
+            titulo=titulo,
+            conteudo=conteudo,
+            autor=request.user,
+            area=area
         )
-
         postagem.save()
-        return redirect('post_detail', pk=postagem.pk)
-    else:
-        return redirect(reverse('nova_postagem'))
+        return redirect('nova_postagem', slug=area_slug)
+    return render(request, 'nova_postagem.html', {'area': area})
     
-def post_detail(request, pk):
+
+
+
+
+
+
+def post_detail(request, area_slug, pk):
     posts = get_object_or_404(Postagem, pk=pk)
     return render(request, 'post_detail.html', {'posts': posts})
+
+
+def area_detail(request, area_slug):
+    return render(request, 'area_detail.html')
+)
